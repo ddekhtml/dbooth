615ac4ca4f76ce4b9fd115e626cec99351c255a4
= <dedeaulad10@gmail.com>
2026-01-19 18:22:18 +0700

    home vue

--- frontend/src/pages/Home.vue
+++ frontend/src/pages/Home.vue
@@ -1,19 +1,35 @@
-<template>
-    <div>
-        {{ session.step }}
-        halo
-        {{ session.isOffline }}
-    </div>
-</template>
-
 <script setup>
-   import { useSessionStore } from '../stores/sessionStore';
-   import { usePhotoStore } from '../stores/photoStore';
+import { useRouter } from 'vue-router'
+import { useSessionStore } from '../stores/sessionStore'
+import { usePhotoStore } from '../stores/photoStore'
 
-   const session = useSessionStore()
+const router = useRouter()
+const session = useSessionStore()
+const photo = usePhotoStore()
 
+function goToFrame() {
+  session.setStep('frame')
+  router.push('/frame')
+}
+
+function resetAll() {
+  session.resetSession()
+  photo.reset()
+}
 </script>
 
-<style scoped>
+<template>
+  <div>
+    <h1>HOME</h1>
 
-</style>
+    <h2>Session Store</h2>
+    <pre>{{ session }}</pre>
+
+    <h2>Photo Store</h2>
+    <pre>{{ photo }}</pre>
+
+    <button @click="goToFrame">Go to Frame Select</button>
+    <br>
+    <button @click="resetAll">Reset Session</button>
+  </div>
+</template>
