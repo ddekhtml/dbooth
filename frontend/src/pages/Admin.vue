e3bf05ae45fdc65f93c8e4875d0ac8d664d093e2
= <dedeaulad10@gmail.com>
2026-01-31 00:15:10 +0700

    pwa, admin, home, ordering the indexed db

--- frontend/src/pages/Admin.vue
+++ frontend/src/pages/Admin.vue
@@ -1,13 +1,109 @@
-<template>
-    <div>
+<script setup>
+import { ref, onMounted } from 'vue'
+import { getAllSubmissions } from '../services/indexesdb'
+import { useSessionStore } from '../stores/sessionStore'
 
-    </div>
-</template>
+const datas = ref([])
+const session = useSessionStore()
+onMounted(async () => {
+  datas.value = await getAllSubmissions()
+})
 
-<script>
-    
 </script>
 
-<style lang="scss" scoped>
+<template>
+  <div class="font-sunday text-center text-6xl text-maroon mt-8">
+    {{ session.eventName }}
+  </div>
+  <div class="font-serif text-maroon text-right mt-3">
+    {{ session.eventDate }}
+  </div>
+  
+  <div class="font-serif text-md mt-3 overflow-x-auto">
+  <table class="min-w-full border-y border-maroon text-left">
+    <thead class="bg-maroon font-extralight text-bg">
+      <tr>
+        <th class="text-center">id</th>
+        <th class="text-center">Created</th>
+        <th class="text-center">Event</th>
+        <th class="text-center">Filtered photo</th>
+        <th class="text-center">Filter</th>
+        <th class="text-center">Raw photos</th>
+        <th class="text-center">Final photo</th>
+        <th class="text-center">Email</th>
+        <th class="text-center">Frame</th>
+      </tr>
+    </thead>
 
-</style>
+    <tbody>
+      <tr
+        v-for="(item, index) in datas"
+        :key="index"
+        class="border-t"
+      >
+        <td>{{ item.id.slice(0,3) }}</td>
+        <td class="text-center">
+        {{
+            new Date(item.created_at).toLocaleTimeString('en-GB', {
+            hour: '2-digit',
+            minute: '2-digit'
+        })
+        }}
+        </td>
+        <td>{{ item.event_name.slice(0,8) }}</td>
+        <td>
+            <div v-if="item.selected_frame">
+                <img
+                v-for="n in item.selected_frame.photoCount"
+                :key="n"
+                :src="item.filtered_photo"
+                class="w-20"
+            >
+            </div>
+            <div v-else>
+                none
+            </div>
+        </td>
+        <td>
+            <p v-if="item.selected_filter">
+                {{ item.selected_filter.id }}
+            </p>
+            <p v-else>null</p>
+            
+        </td>
+        <td>
+            <div v-if="item.selected_frame">
+                <img
+                v-for="n in item.selected_frame.photoCount"
+                :key="n"
+                :src="item.raw_photos"
+                class="w-20"
+            >
+            </div>
+            <div v-else>
+                none
+            </div>
+        </td>
+        <td>
+            <img src="/frames/wedding/zidni-aike/grey.png" alt="null" class="w-20 my-2">
+        </td>
+        <td>
+            <p v-if="item.email">
+                 {{ item.email.slice(0,5) }}
+            </p>
+            <p v-else>null</p>
+        </td>
+        <td>
+            <p v-if="item.selected_frame">
+                {{ item.selected_frame.id }}
+            </p>
+            <p v-else>null</p>
+            
+        </td>
+      </tr>
+    </tbody>
+
+  </table>
+</div>
+
+</template>
--- frontend/src/pages/Home.vue
+++ frontend/src/pages/Home.vue
@@ -4,7 +4,7 @@ import { useSessionStore } from '../stores/sessionStore'
 import { usePhotoStore } from '../stores/photoStore'
 import { v4 as uuidv4 } from 'uuid'
 import { clearAllSubmissions, saveSubmission } from '../services/indexesdb'
-import {onMounted} from 'vue'
+import { onMounted } from 'vue'
 const router = useRouter()
 const photo = usePhotoStore()
 const session = useSessionStore()
@@ -14,16 +14,18 @@ async function startEvent() {
   const time = new Date().toISOString()
   await saveSubmission({
     id : setId, 
-    createdAt : time,
-    eventId : session.eventId, 
-    eventName: session.eventName, 
-    eventDate: session.eventDate, 
-    selectedFilter: null,
-    filteredPhoto: null, 
-    finalPhoto: null, 
+    created_at : time,
+    event_id : session.eventId, 
+    event_name : session.eventName, 
+    event_date: session.eventDate, 
+    raw_photos : null,
+    selected_filter: null,
+    filtered_photos: null, 
+    final_photo: null, 
+    selected_frame: null, 
     printed : false, 
     emailed : false, 
-    email : '', 
+    email : null,
   })
   photo.setCurrentSubmissionId(setId)
   photo.setCreated(time)
@@ -31,15 +33,53 @@ async function startEvent() {
   router.push('/frame')
 }
 
-// onMounted(() => {
-//   clearAllSubmissions()
-// })
+onMounted(() => {
+  session.resetSession()
+})
+
+function toAdmin(){
+  router.push('/admin')
+}
 
 </script>
 
 <template>
-  <div>
-    <h1>HOME</h1>
-    <button @click="startEvent">Start Photobooth</button>
+  
+  <div class="h-dvh w-full flex flex-col text-maroon font-sunday overflow-hidden">
+
+    <h1 class="text-9xl self-center mt-25">
+      <button @click="toAdmin">
+        ZIDNI & AIKE
+      </button>
+      
+    </h1>
+
+    <p class="self-center text-3xl mt-16 tracking-wide">
+      <button @click="clearAllSubmissions()">
+        07.02.2026
+      </button>
+    </p>
+
+    <div class="mt-10 self-center bg-maroon font-serif text-bg text-4xl px-20 pt-4 pb-5 rounded-full">
+      <button @click="startEvent">Mulai</button>
+    </div>
+
+    <!-- photostrip -->
+    <div class="mt-auto overflow-hidden w-full">
+      <div class="relative font-serif text-right text-md pr-23 mb-2">
+        powered by
+        <img
+          src="/img/dbooth.png"
+          alt=""
+          class="absolute right-0 top-1/2 -translate-y-1/2 w-25 block"
+        >
+      </div>
+      <img src="/img/love.png" alt="" class="w-full block">
+    </div>
+
   </div>
+
+
+      <!-- <button @click="startEvent">Start Photobooth</button> -->
+ 
 </template>
